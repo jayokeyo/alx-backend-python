430f033c3d9737a44c814538c56e2285dfbd1a24
jayokeyo <jaykopiyo@gmail.com>
2023-10-05 20:29:50 +0000

    Added 3-to_str.py

---
+++ 3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Python - Variable Annotated function to_str
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Converts a floating-point number to a sring
+    '''
+    return str(n)i
