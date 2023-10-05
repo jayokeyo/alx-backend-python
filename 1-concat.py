b71b81198c379d9b3c4db50bdef9fe930e70ae04
jayokeyo <jaykopiyo@gmail.com>
2023-10-05 20:25:56 +0000

    Added 1-concat.py

---
+++ 1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Python - Variable Annotated function concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Concatenates two strings and return a string
+    '''
+    return str1 + str2
