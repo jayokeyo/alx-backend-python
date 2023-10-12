6e55bf5f6f176714aa8c8c19d6721f44e1aefbc1
jayokeyo <jaykopiyo@gmail.com>
2023-10-12 15:10:13 +0000

    Pycodestyle requirement

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,6 +14,8 @@ from typing import List
 
 
 wait_random = __import__("0-basic_async_syntax").wait_random
+
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     takes in 2 int arguments (in this order): n and max_delay.
